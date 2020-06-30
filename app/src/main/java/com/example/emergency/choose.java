fad1b4898a9036b297d9aa02f4113dd9cf01aa36
AaronShabanian <shabanian@chapman.edu>
2020-06-30 16:38:59 -0700

    Added new part

--- app/src/main/java/com/example/emergency/choose.java
+++ app/src/main/java/com/example/emergency/choose.java
@@ -2,13 +2,27 @@ package com.example.emergency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class choose extends AppCompatActivity {
-
+    private Button texts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        texts= findViewById(R.id.text);
+        texts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totext();
+            }
+        });
+    }
+    public void totext(){
+        Intent intent= new Intent(this, textUpload.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/emergency/textUpload.java
@@ -0,0 +1,14 @@
+package com.example.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class textUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_upload);
+    }
+}
