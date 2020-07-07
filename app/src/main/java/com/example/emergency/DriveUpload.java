4a97834580ed237d929bb9ae063f2e6f6d5a9bd5
AaronShabanian <shabanian@chapman.edu>
2020-07-07 13:01:59 -0700

    Beggining of Google Drive Implementation.

---
+++ app/src/main/java/com/example/emergency/DriveUpload.java
@@ -0,0 +1,22 @@
+package com.example.emergency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class DriveUpload extends AppCompatActivity {
+    private WebView drive;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drive_upload);
+        drive=findViewById(R.id.drive);
+        WebSettings webSettings = drive.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        drive.setWebViewClient(new WebViewClient());
+        drive.loadUrl("https://drive.google.com/drive/u/0/my-drive");
+    }
+}
--- app/src/main/java/com/example/emergency/Scan.java
+++ app/src/main/java/com/example/emergency/Scan.java
@@ -2,13 +2,30 @@ package com.example.emergency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Scan extends choose {
-
+    private Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+        start= findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("myTag", "Data Saved");
+
+                starts();
+            }
+        });
+    }
+    public void starts(){
+        Intent intent= new Intent(this, DriveUpload.class);
+        startActivity(intent);
     }
 }
