12bda9da7ff385c32be3f7284874acbfcdb7ab2d
AaronShabanian <shabanian@chapman.edu>
2020-07-09 13:01:44 -0700

    IDK

--- app/build/generated/source/buildConfig/debug/com/example/emergency/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/emergency/BuildConfig.java
@@ -7,6 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.example.emergency";
   public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
 }
