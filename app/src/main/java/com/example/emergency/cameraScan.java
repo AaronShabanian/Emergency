f2282b7fc9581d9d1c598d4ff6913c9bd6d54b3b
AaronShabanian <shabanian@chapman.edu>
2020-07-15 13:44:12 -0700

    Meh

--- app/src/main/java/com/example/emergency/cameraScan.java
+++ app/src/main/java/com/example/emergency/cameraScan.java
@@ -36,6 +36,7 @@ public class cameraScan extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_scan);
         mImageView = (ImageView) findViewById(R.id.result);
+        Log.i("error","test");
 
     }
 
